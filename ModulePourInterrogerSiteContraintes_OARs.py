1de4e90ab48c13491e9ff037a7f77bff663da08c
romaincayez <romain.cayez@gmail.com>
2024-01-22 15:42:08 +0100

    init commit

---
+++ ModulePourInterrogerSiteContraintes_OARs.py
@@ -0,0 +1,37 @@
+import requests
+
+URL = "http://vrttools:1337/api/"
+
+def check_url(url):
+    try:
+        r = requests.get(url)
+        return  r.status_code == 200
+    except:
+        return False
+
+def get_indication(location='', organ='', fraction='', patient=''):
+    url = URL + 'indication?'
+    if location:
+        print('location : ', location)
+        url += 'location=' + location
+    if organ:
+        url += '&organ=' + organ
+    if fraction:
+        url += '&fraction=' + fraction
+    if patient:
+        url += '&patient=' + patient
+
+    print(check_url(url))
+
+    if check_url(url):
+        print('ok')
+        r = requests.get(url)
+        return r.json()
+    else:
+        return {}
+
+doseVessie = get_indication(location='ABDOMEN',
+                            organ='A_Pulmonaire',
+                            fraction='1',
+                            patient='Adulte')
+print(doseVessie)
---
+++ temp.py
@@ -0,0 +1,31 @@
+import os, sys
+pid_file_path = os.path.join(os.environ.get('userprofile'), 'AppData', 'Local', 'Temp', 'raystation.pid')
+
+with open(pid_file_path) as f:
+    os.environ['RAYSTATION_PID'] = f.read()
+
+script_client_path = r'C:\Program Files\RaySearch Laboratories\RayStation 12A-SP1\ScriptClient'
+sys.path.append(script_client_path)
+
+from connect import *
+
+plan = get_current("Plan")
+case = get_current("Case")
+
+# print('toto : ', plan.TreatmentCourse.EvaluationSetup.EvaluationFunctions[0])
+
+
+for i in range(len(plan.TreatmentCourse.EvaluationSetup.EvaluationFunctions)):
+    roiName = plan.TreatmentCourse.EvaluationSetup.EvaluationFunctions[i].ForRegionOfInterest.Name
+    if not case.PatientModel.RegionsOfInterest[roiName].Type == 'Ptv':
+        while 'ptv' in plan.TreatmentCourse.EvaluationSetup.EvaluationFunctions[i].ForRegionOfInterest.Name.lower():
+            plan.TreatmentCourse.EvaluationSetup.DeleteClinicalGoal(FunctionToRemove=plan.TreatmentCourse.EvaluationSetup.EvaluationFunctions[i])
+
+
+
+# try:
+#     while len(plan.TreatmentCourse.EvaluationSetup.EvaluationFunctions) != 0:
+#         plan.TreatmentCourse.EvaluationSetup.DeleteClinicalGoal(
+#             FunctionToRemove=plan.TreatmentCourse.EvaluationSetup.EvaluationFunctions[0])
+# except:
+#     print('tous les clinical goals sont supprimés')
